4279e300ec9f98e945acb42c479d80ff4e47ff58
Eszter Kiskarpati <keszi94@gmail.com>
2024-11-17 21:23:52 +0000

    Add test code for addition

---
+++ script.t.js
@@ -0,0 +1,16 @@
+// const { TestScheduler } = require("jest")
+
+// function addTwoNumbers(a, b) {
+//     return a + b
+// };
+
+// addTwoNumbers(20, 22);
+
+// describe("calculator tests", () => {
+//     describe("addition tests", () => {
+//         test("should return 42", () => {
+//             expect(addTwoNumbers(20,22)).toBe(42);
+//         });
+//     });
+// });
+
